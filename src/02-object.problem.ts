c486cf7b3a9304f6807dd63a6fdfdb7977adae33
muriukialex <alexmuriukimaina254@gmail.com>
2024-08-02 08:49:48 +0300

    - Add solution for 02-object.problem

--- src/02-object.problem.ts
+++ src/02-object.problem.ts
@@ -29,12 +29,64 @@ export const fetchStarWarsPersonName = async (id: string) => {
     ).then(res => res.json());
 
     const parsedData = PersonResult.parse(data);
-    return parsedData.name;
+    return parsedData;
 };
 
 // TESTS
 
-it('Should return the name', async () => {
-    expect(await fetchStarWarsPersonName('1')).toEqual('Luke Skywalker');
-    expect(await fetchStarWarsPersonName('2')).toEqual('C-3PO');
+it('Should return the expected person properties', async () => {
+    expect(await fetchStarWarsPersonName('1')).toMatchObject({
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        hair_color: 'blond',
+        skin_color: 'fair',
+        eye_color: 'blue',
+        birth_year: '19BBY',
+        gender: 'male',
+        homeworld: 'https://swapi.dev/api/planets/1/',
+        films: [
+            'https://swapi.dev/api/films/1/',
+            'https://swapi.dev/api/films/2/',
+            'https://swapi.dev/api/films/3/',
+            'https://swapi.dev/api/films/6/',
+        ],
+        species: [],
+        vehicles: [
+            'https://swapi.dev/api/vehicles/14/',
+            'https://swapi.dev/api/vehicles/30/',
+        ],
+        starships: [
+            'https://swapi.dev/api/starships/12/',
+            'https://swapi.dev/api/starships/22/',
+        ],
+        created: '2014-12-09T13:50:51.644000Z',
+        edited: '2014-12-20T21:17:56.891000Z',
+        url: 'https://swapi.dev/api/people/1/',
+    });
+    expect(await fetchStarWarsPersonName('2')).toMatchObject({
+        name: 'C-3PO',
+        height: '167',
+        mass: '75',
+        hair_color: 'n/a',
+        skin_color: 'gold',
+        eye_color: 'yellow',
+        birth_year: '112BBY',
+        gender: 'n/a',
+        homeworld: 'https://swapi.dev/api/planets/1/',
+        films: [
+            'https://swapi.dev/api/films/1/',
+            'https://swapi.dev/api/films/2/',
+            'https://swapi.dev/api/films/3/',
+            'https://swapi.dev/api/films/4/',
+            'https://swapi.dev/api/films/5/',
+            'https://swapi.dev/api/films/6/',
+        ],
+        species: ['https://swapi.dev/api/species/2/'],
+        vehicles: [],
+        starships: [],
+        created: '2014-12-10T15:10:51.357000Z',
+        edited: '2014-12-20T21:17:50.309000Z',
+        url: 'https://swapi.dev/api/people/2/',
+    });
 });
