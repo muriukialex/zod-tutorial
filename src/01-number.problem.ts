06f17ce046996f0f90bc36dd394de4561935116f
muriukialex <alexmuriukimaina254@gmail.com>
2024-08-02 07:50:43 +0300

    - Add solution for 01-number.problem

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -1,21 +1,25 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 //       ^ 🕵️‍♂️
 
+const parseNumber = z.number();
+
 export const toString = (num: unknown) => {
-  return String(num);
+    const result = parseNumber.parse(num);
+
+    return String(result);
 };
 
 // TESTS
 
-it("Should throw a runtime error when called with not a number", () => {
-  expect(() => toString("123")).toThrowError(
-    "Expected number, received string",
-  );
+it('Should throw a runtime error when called with not a number', () => {
+    expect(() => toString('123')).toThrowError(
+        'Expected number, received string'
+    );
 });
 
-it("Should return a string when called with a number", () => {
-  expect(toString(1)).toBeTypeOf("string");
+it('Should return a string when called with a number', () => {
+    expect(toString(1)).toBeTypeOf('string');
 });
